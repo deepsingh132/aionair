7bbaf58fdc6e9daed96c6345f91da3511d447a2c
Mandeep Singh <mandeeparora132@gmail.com>
2024-07-08 16:47:55 +0530

    Add null check to ProfileCard component

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
   images: {
     remotePatterns: [
       {
